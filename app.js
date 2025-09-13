5c51c161ec1971385e39048adf0a642fd885572a
zumer305 <zumerniaz305@gmail.com>
2025-09-14 02:27:28 +0500

    update

---
+++ app.js
@@ -0,0 +1,52 @@
+// trim method {remove spaces (before & after just)            .trim()...new string banti}
+// immutable(cant change) hoti string
+
+
+
+
+
+
+
+// let pass=prompt("zumer");
+// let npass=pass.trim();
+// console.log(npass);
+
+
+// upercase/lowerase
+let name="Zumer";
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+
+
+// indexOf (argument)
+console.log(name.indexOf("u"));
+// no index found -1
+console.log(name.indexOf("k"));
+
+
+// method chaining
+let m="      hello";
+console.log(m.trim().toUpperCase());
+
+// slice
+let sl="zumer niaz";
+// index start sa end
+console.log(sl.slice(1));
+// index start and end
+console.log(sl.slice(1,7));
+// length-num
+console.log(sl.slice(-2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
